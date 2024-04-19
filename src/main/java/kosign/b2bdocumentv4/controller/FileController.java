fef5e7f11fb5c1a275890aaf36dcd55b9e909415
sovita <sovita>
2024-04-19 15:39:25 +0700

    done

--- src/main/java/kosign/b2bdocumentv4/controller/FileController.java
+++ src/main/java/kosign/b2bdocumentv4/controller/FileController.java
@@ -8,12 +8,16 @@ import kosign.b2bdocumentv4.payload.login.response.ApiResponse;
 import kosign.b2bdocumentv4.service.doc_file.FileServiceImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -45,15 +49,36 @@ public class FileController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/view_image")
+    @GetMapping("/view_files")
     public ResponseEntity<?> fileUpload(@RequestParam String fileName) throws IOException {
+        try {
+            Path path = Paths.get(file_path + "/" + fileName);
+            // Read the Excel file as InputStream
+            InputStreamResource resource = new InputStreamResource(new FileInputStream(path.toFile()));
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+
+            // Set the content type to application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+            headers.setContentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(resource);
+        } catch (FileNotFoundException e) {
+            throw new NotFoundExceptionClass("This file does not exist.");
+        }
+    }
+
+    @GetMapping("/view_images")
+    public ResponseEntity<?> getImagesByFileName(@RequestParam String fileName) throws NotFoundExceptionClass {
         try{
             Path path = Paths.get(file_path + "/" + fileName);
-            //get and response as image
-            Resource file = new ByteArrayResource(Files.readAllBytes(path));
+            Resource file =  new ByteArrayResource(Files.readAllBytes(path));
+
             return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(file);
         }catch (Exception e){
-            throw new NotFoundExceptionClass("this file isn't exist.");
+            throw new NotFoundExceptionClass ("this file isn't exist.");
         }
     }
 
@@ -67,4 +92,16 @@ public class FileController {
                 .build();
         return ResponseEntity.ok(response);
     }
+
+    @DeleteMapping("/delete_file")
+    public ResponseEntity<ApiResponse> deleteFile(@RequestParam Integer fileId) throws IOException {
+        fileService.deletefile(fileId);
+        ApiResponse<List<DocumentFile>> response = ApiResponse.<List<DocumentFile>>builder()
+                .status(200)
+                .message("Deleted Successfully.")
+                .date(LocalDateTime.now())
+                .build();
+        return ResponseEntity.ok(response);
+    }
+
 }
--- src/main/java/kosign/b2bdocumentv4/entity/doc_articles/DocumentArticlesRepository.java
+++ src/main/java/kosign/b2bdocumentv4/entity/doc_articles/DocumentArticlesRepository.java
@@ -14,5 +14,8 @@ public interface DocumentArticlesRepository extends JpaRepository<DocumentArticl
            """,nativeQuery = true)
     List<DocumentArticles> getByDepartmentId(String dep_id);
 
-
+    @Query(value = """
+           select da.file_article_id from stdy.doc_articles da where da.id = :id
+           """,nativeQuery = true)
+    String getFileArticleById(Long id);
 }
