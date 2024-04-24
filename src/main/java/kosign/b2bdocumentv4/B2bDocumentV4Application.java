0ae19358741645a711bd05e4785d86180a18a629
GuoErr <43460012+DaraBoth@users.noreply.github.com>
2024-04-24 16:30:17 +0700

    update file

--- src/main/java/kosign/b2bdocumentv4/B2bDocumentV4Application.java
+++ src/main/java/kosign/b2bdocumentv4/B2bDocumentV4Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class B2bDocumentV4Application {
-
     public static void main(String[] args) {
         SpringApplication.run(B2bDocumentV4Application.class, args);
     }
-
 }
